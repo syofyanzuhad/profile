d9314bd10358b9ae4b13852d6bbda350d23cd7f7
syofyanzuhad <sofyanzuhad2@gmail.com>
2022-11-01 18:10:11 +0700

    fix: vercel/analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,8 @@ import { ThemeProvider } from 'styled-components';
 import { useEffect } from 'react';
 import { darkTheme, lightTheme } from 'Styles/themes';
 import { useDispatch, useSelector } from 'react-redux';
+import { Analytics } from '@vercel/analytics/react';
+
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
