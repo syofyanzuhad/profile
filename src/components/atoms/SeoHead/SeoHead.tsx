ed16613aee47d7af00e54715cde679e20142e580
syofyanzuhad <sofyanzuhad2@gmail.com>
2022-12-09 17:15:04 +0700

    update: meta-head

--- src/components/atoms/SeoHead/SeoHead.tsx
+++ src/components/atoms/SeoHead/SeoHead.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
 import Head from 'next/head';
 
+import website from 'Public/assets/website.png';
+
 export interface HeadProps {
 	title: string;
 	description: string;
@@ -17,9 +19,9 @@ export interface HeadProps {
 const SeoHead: FC<HeadProps> = ({
 	title,
 	description,
-	author = 'Jacob Herper',
+	author = 'Syofyan Zuhad',
 	keywords,
-	image = 'https://res.cloudinary.com/jacobherper/image/upload/c_fill,g_north,h_1080,w_1920/v1587315694/herper-io.png ',
+	image = '/assets/website.png',
 	meta,
 }) => {
 	return (
@@ -30,7 +32,7 @@ const SeoHead: FC<HeadProps> = ({
 			<meta name="og:title" content={title} />
 			<meta name="og:description" content={description} />
 			<meta name="og:type" content="website" />
-			<meta name="og:url" content="https://jacobherper.com/" />
+			<meta name="og:url" content="https://syofyanzuhad.my.id/" />
 			<meta name="twitter:card" content="summary" />
 			<meta name="twitter:title" content={title} />
 			<meta name="twitter:description" content={description} />
@@ -46,13 +48,13 @@ const SeoHead: FC<HeadProps> = ({
 			<script
 				type="application/ld+json"
 				dangerouslySetInnerHTML={{
-					__html: `{"@context":"http://schema.org","@type":"WebSite","name":"JacobHerper.com","alternateName":"Jacob Herper","url":"https://jacobherper.com/","description":"Front-End Software Engineer with a focus on React.js and TypeScript. I have more than 10 years experience working in software engineering.","image":"${image}"}`,
+					__html: `{"@context":"http://schema.org","@type":"WebSite","name":"Syofyanzuhad.my.id","alternateName":"Syofyan Zuhad","url":"https://syofyanzuhad.my.id/","description":"Back-End Software Engineer with a focus on Laravel and php. I have more than 2 years experience working in software engineering.","image":"${image}"}`,
 				}}
 			/>
 			<script
 				type="application/ld+json"
 				dangerouslySetInnerHTML={{
-					__html: `{"@context":"http://schema.org","@type":"Person","image":"https://res.cloudinary.com/jacobherper/image/upload/v1587315576/jacob_herper.jpg","name":"Jacob Herper","alternateName":"JakeHerp","url":"https://herper.io/","jobTitle":["Senior Software Engineer","Frontend Engineer", "Front-End Developer", "Web Developer"],"hasOccupation":{"@type":"Occupation","name":"Senior Software Engineer","occupationLocation":{"@type":"City","name":"London"},"estimatedSalary":[{"@type":"MonetaryAmountDistribution","name":"base","currency":"GBP","duration":"P1H","percentile10":"74","percentile25":"80","median":"90","percentile75":"100","percentile90":"106"}],"description":"Develops web applications and websites using JavaScript, React and HTML5.","skills":"HTML5, CSS, JavaScript, React, PHP, Sass, Less, Node.js, Express.js, Vue.js, Gatsby, Next.js, JavaScript Frameworks, Git, Github, NPM, SEO, CMS, WordPress, Joomla, Drupal, E-Commerce","alternateName":["Frontend Developer","Full Stack Developer","Frontend Engineer"],"responsibilities":["App Development","Web Design","Website Development","JavaScript Development","WordPress Development","CMS Development","Frontend Development","Full Stack Development","Online Marketing","SEO Services","Web Developer","Website Maintenance"]},"sameAs":["http://linkedin.com/in/jacobherper","http://twitter.com/jakeherp","https://github.com/jakeherp","https://www.facebook.com/jakeherp","https://instagram.com/jakeherp"],"brand":[{"@type":"Brand","name":"Software Engineer","alternateName":"Front-End Engineer"},{"@type":"Brand","name":"JakeHerp","alternateName":"Jacob Herper"}],"memberOf":[{"@type":"Organization","url":"https://creativ.agency/","name":"Creativ Agency Ltd","alternateName":"Creativ Advertising Agency"},{"@type":"Organization","url":"https://www.dennis.co.uk/","name":"Dennis Digital","alternateName":"Dennis"}],"homeLocation":{"@type":"City","name":"London"},"owns":{"@type":"Organization","url":"https://creativ.agency/","name":"Creativ Agency Ltd","alternateName":"Creativ Advertising Agency","sameAs":"https://www.linkedin.com/company/creativ.agency"},"alumniOf":{"@type":"Organization","url":"https://www.thespecialistworks.com/","name":"The Specialist Works"},"nationality":{"@type":"Country","name":"Germany"},"birthDate":"1990-11-06","birthPlace":{"@type":"Place","address":{"@type":"PostalAddress","addressLocality":"Oelsnitz","postalCode":"08606","addressCountry":"Germany"}},"parent":[{"name":"Norbert Herper","jobTitle":"Chef","birthDate":"1967-08-22"},{"name":"Anne-Kristin Herper","birthDate":"1968-02-26"}],"spouse":{"name":"Cinthia Cid Paz","birthDate":"1990-09-19","nationality":{"@type":"Country","name":"Mexico"},"birthPlace":{"@type":"Place","address":{"@type":"PostalAddress","addressLocality":"Toluca","addressCountry":"Mexico"}}}}`,
+					__html: `{"@context":"http://schema.org","@type":"Person","image":"https://res.cloudinary.com/jacobherper/image/upload/v1587315576/jacob_herper.jpg","name":"Syofyan Zuhad","alternateName":"JakeHerp","url":"https://herper.io/","jobTitle":["Senior Software Engineer","Backend Engineer", "Back-End Developer", "Web Developer"],"hasOccupation":{"@type":"Occupation","name":"Senior Software Engineer","occupationLocation":{"@type":"City","name":"London"},"estimatedSalary":[{"@type":"MonetaryAmountDistribution","name":"base","currency":"GBP","duration":"P1H","percentile10":"74","percentile25":"80","median":"90","percentile75":"100","percentile90":"106"}],"description":"Develops web applications and websites using JavaScript, React and HTML5.","skills":"HTML5, CSS, JavaScript, React, PHP, Sass, Less, Node.js, Express.js, Vue.js, Gatsby, Next.js, JavaScript Frameworks, Git, Github, NPM, SEO, CMS, WordPress, Joomla, Drupal, E-Commerce","alternateName":["Backend Developer","Full Stack Developer","Backend Engineer"],"responsibilities":["App Development","Web Design","Website Development","JavaScript Development","WordPress Development","CMS Development","Backend Development","Full Stack Development","Online Marketing","SEO Services","Web Developer","Website Maintenance"]},"sameAs":["http://linkedin.com/in/syofyan-zuhad","http://twitter.com/syofyan_zuhad","https://github.com/syofyanzuhad","https://www.facebook.com/syofyan_zuhad","https://instagram.com/syofyan_zuhad"],"brand":[{"@type":"Brand","name":"Software Engineer","alternateName":"Back-End Engineer"},{"@type":"Brand","name":"Syofyanzuhad","alternateName":"Syofyan Zuhad"}],"memberOf":[{"@type":"Organization","url":"https://creativ.agency/","name":"Creativ Agency Ltd","alternateName":"Creativ Advertising Agency"},{"@type":"Organization","url":"https://www.dennis.co.uk/","name":"Dennis Digital","alternateName":"Dennis"}],"homeLocation":{"@type":"City","name":"London"},"owns":{"@type":"Organization","url":"https://creativ.agency/","name":"Creativ Agency Ltd","alternateName":"Creativ Advertising Agency","sameAs":"https://www.linkedin.com/company/creativ.agency"},"alumniOf":{"@type":"Organization","url":"https://www.thespecialistworks.com/","name":"The Specialist Works"},"nationality":{"@type":"Country","name":"Germany"},"birthDate":"1990-11-06","birthPlace":{"@type":"Place","address":{"@type":"PostalAddress","addressLocality":"Oelsnitz","postalCode":"08606","addressCountry":"Germany"}},"parent":[{"name":"Norbert Herper","jobTitle":"Chef","birthDate":"1967-08-22"},{"name":"Anne-Kristin Herper","birthDate":"1968-02-26"}],"spouse":{"name":"Cinthia Cid Paz","birthDate":"1990-09-19","nationality":{"@type":"Country","name":"Mexico"},"birthPlace":{"@type":"Place","address":{"@type":"PostalAddress","addressLocality":"Toluca","addressCountry":"Mexico"}}}}`,
 				}}
 			/>
 			<script async src="https://cdn.splitbee.io/sb.js"></script>
